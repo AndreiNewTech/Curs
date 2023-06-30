6a5d9a0eafdf209be80228f90b36cf94fa60e66f
AndreiDudeOne <duhanesandrei@gmail.com>
2023-06-30 15:39:22 +0300

    Operatori si if else

--- 27-06-2023/operators/operators.js
+++ 27-06-2023/operators/operators.js
@@ -7,7 +7,7 @@
 // Logical
 
 
-//  Assignment and Addition
+// 1  Assignment and Addition
   //  Asignarea unor valori unor varibile, 
   //  addition + concateneaza variabile de tip string
 
@@ -17,13 +17,18 @@
 
   age += 8;
   // age = age + 8;
+  age -= 8
+  // age = age - 8
+
+  // *= /= 
+
   console.log('Varsta mea o sa fie la un moment dat, sper', age);
 
   const persoana = firstName + " " + lastName + " " + age + " ani";
   console.log(persoana);
 
 
-// Addition ( ideea de coercitie, coercion)
+// 2 Addition ( ideea de coercitie, coercion)
   console.log('Anghel' + ' Gabriel');
   console.log(5 + 5);
   console.log(5 + '5');
@@ -31,7 +36,7 @@
   console.log(+'10');
 
   
-// Arithmetic
+// 3 Arithmetic
   // Addition
   10 + 5;
 
@@ -61,6 +66,7 @@
   let y = 10;
   // y = y - 1;
   y--;
+
   console.log(y, 'Y value');
 
   let sum = (((1 + 2 + 3 + 4 + 5) / 2) / 2) ** 2;
@@ -68,3 +74,83 @@
   console.log(sum);
   const my_sum = 'My sum is ' + sum;
   console.log(my_sum);
+
+
+  // Comparison (is it true or false)
+
+  // Equals ===
+  5 === 5; // true
+  5 === 6; // false
+  5 === 'a' // false
+  'abc' === 'abc' // true
+
+  const ten = 10;
+  const tenB = 10;
+  const twenty = 20;
+
+  ten === twenty; // false
+  ten === ten; // true
+  ten === tenB; // true
+
+  null === undefined; // false
+  true === true; // true
+  true === false; // false
+
+  // Equals == Nu folositi!!!
+  2 == '2'; // true
+  0 == false; // true  ( 0 reprezinta false)
+  1 == false; // false ( 1 reprezinta true)
+  "" == false; // true
+  undefined == null; // true
+
+  // Falsy values ex: 0, "", undefined, null, NaN, false
+  // Truthy toate celelate (numere, string-uri etc)
+
+
+  // Inegalitate !== (opusul ===)
+  5 !== 5; // false
+  5 !== 6; // true
+  ten !== tenB; // false 
+
+
+  // Greater than > & Lesser than < 
+  5 > 7; // false
+  5 < 10; // true
+
+  const hundred = 100;
+  const thousand = 1000;
+
+  hundred < thousand; // true
+  thousand > hundred; // true
+
+  hundred + 901 > thousand; // true
+  thousand < hundred + 1000 // true
+
+  5 <= 5; // true
+  5 >= 5; // true
+
+  'string' > 2 // false -> string este tranformat in NaN si toate NaN in comparatie cu ceva sunt false
+
+  '3' > 2 // true -> '3' transformat in 3, si 3 > 2
+  '3' < 2 // false
+
+  // Logical operations
+
+  // AND &&
+  true && true // true
+  true && false // false
+  false && true // false
+  false && false // false
+
+  // OR ||
+  true || false // true
+  true || true // true
+  false || true // true
+  false || false // false
+
+  // NOT !
+  !true // false
+  !false // true
+
+
+  Boolean(5 && '5');
